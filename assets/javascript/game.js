64a52ac2b8410afc62a12a5669441eee9254605b
Benjamin Morrow <morrowbenjamin42@gmail.com>
2019-07-01 20:41:02 -0400

    Added display of character sprites and HP field.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,64 @@
-var characters = {
-    dnc: {
+var charChosen = false;
+var characters = [
+    {
         class : "dancer",
         att : 20,
         hp : 20,
-        src : "assets/img/sprites/dancer.png"
+        src : "assets/img/bard.png",
+        val : "dnc",
     },
-    brd: {
+    {
         class : "bard",
         att : 25,
         hp : 15,
+        src: "assets/img/bard.png",
+        val : "brd",
     },
-    mch: {
+    {
         class : "machinist",
         att : 15,
         hp : 25,
+        src : "assets/img/mch.png",
+        val : "mch",
     },
-};
-Object.keys(characters).forEach(function(key){
-    console.log(key, characters[key]);
-    var imageCharacter = $("<img>");
-    imageCharacter.addClass("character-image col-md-4");
-    imageCharacter.attr("value", Object.keys(characters));
-    imageCharacter.attr("src", "http://placehold.it/300x300");
-    $("#characters").append(imageCharacter);
-});
+];
+$( document ).ready(){
+    if(charChosen !== true)
+    {
+        characters.forEach(function(character){
+            var source = characters.map(a => a.src);
+            var val = characters.map(a => a.val);
+            var att = characters.map(a => a.att);
+            var hp = characters.map(a => a.hp);
+            var imageCharacter = $("<img>");
+            imageCharacter.addClass("character-image col-md-4");
+            $("#characters").append(imageCharacter);
+            $('img').each(function(index){
+                $(this).attr("value", val[index]);
+            });
+            $('img').each(function(index){
+                $(this).attr("src", source[index]);
+            });
+            var charHP = $("<p>");
+            charHP.addClass("character-HP col-md-4 text-center");
+            $("#characterHP").append(charHP);
+            $('p').each(function(index){
+                $(this).text("HP : " + hp[index]);
+            });
+            // var charAtt = $("<p>");
+            // charAtt.addClass("character-Att col-md-4 text-center");
+            // $("#characterAtt").append(charAtt);
+            // $('p').each(function(index){
+            //     $(this).text("Att : " + att[index]);
+            // });
+        });
+        $('.character-image').on("click", function(){
+            charChosen = true;
+            console.log(charChosen);
+        });
+    }
+    else
+    {
+
+    }
+)};
