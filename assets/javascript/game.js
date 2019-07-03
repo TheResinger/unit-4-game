e508ea2453af5449d35524c8954c1d0c6f9ec0ef
TheResinger <49324943+TheResinger@users.noreply.github.com>
2019-07-03 14:12:55 -0400

    Add files via upload
    Added functionality for damage calculation.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,24 +2,30 @@ var charChosen = false;
 var enemyChosen = false;
 var chosenClass = [];
 var chosenEnemy = [];
+var damageStore = [];
+var damageCalc = 0;
+var playerHP = 1;
 var characters = [
     {
         class : "Dancer",
-        att : 20,
+        minAtt : 90,
+        maxAtt : 110,
         hp : 20,
         src : "assets/img/dnc.png",
         val : "dnc",
     },
     {
         class : "Bard",
-        att : 100,
+        minAtt : 90,
+        maxAtt : 110,
         hp : 15,
         src: "assets/img/brd.png",
         val : "brd",
     },
     {
         class : "Machinist",
-        att : 15,
+        minAtt : 90,
+        maxAtt : 110,
         hp : 25,
         src : "assets/img/mch.png",
         val : "mch",
@@ -57,7 +63,8 @@ $("#charSheet").append($("<div>", {"id" : "characterAtt", "class" : "row"}));
 characters.forEach(function(){
     var charSource = characters.map(a => a.src);
     var charVal = characters.map(a => a.val);
-    var charAtt = characters.map(a => a.att);
+    var charMinAtt = characters.map(a => a.minAtt);
+    var charMaxAtt = characters.map(a => a.maxAtt);
     var charHp = characters.map(a => a.hp);
     var charClass = characters.map(a => a.class);
     $("#characters").append($("<img>", {"class" : "character-image col-md-4"}));
@@ -77,108 +84,130 @@ characters.forEach(function(){
     });
     $("#characterAtt").append($("<p>", {"class" : "character-Att col-md-4 text-center"}));
     $("#characterAtt").find("p").each(function(index){
-        $(this).text("Att : " + charAtt[index]);
+        $(this).text("Attack : " + charMinAtt[index] + " - " + charMaxAtt[index]);
     });
 });
 
+if(playerHP >= 0)
+{
 
-$(document).on("click", '.character-image', function(){
-    console.log($(this).attr("value"));
-    chosenClass.push($(this).attr("value"));
-    charChosen = true;
-    $("#charSheet").remove();
-    $(".jumbotron").remove();
-    // Generate Game Space Div to nest everything in
-    $(".container").append($('<div>',{ "id" : "gameSpace"}));
+    $(document).on("click", '.character-image', function(){
+        // console.log($(this).attr("value"));
+        chosenClass.push($(this).attr("value"));
+        charChosen = true;
+        $("#charSheet").remove();
+        $(".jumbotron").remove();
+        // Generate Game Space Div to nest everything in
+        $(".container").append($('<div>',{ "id" : "gameSpace"}));
 
-    // Create Div for chosen player sprite to reside 
-    $("#gameSpace").append($('<div>',{ "class" : "row", "id" : "playerDiv"}));
+        // Create Div for chosen player sprite to reside 
+        $("#gameSpace").append($('<div>',{ "class" : "row", "id" : "playerDiv"}));
 
-    // Grabs the chosen classes image source, and hp and assigns it into a div sorted with bootstrap grid
-    var returnSrc = characters.filter(obj => obj.val == chosenClass ).map(obj => obj.src);
-    var returnHP = characters.filter(obj => obj.val == chosenClass ).map(obj => obj.hp);
-    $("#playerDiv").append($('<div>',{ "class" : "col-md-4"}));  //Blank Div for padding
-    $("#playerDiv").append($('<img>',{ "class" : "col-md-4","id" : "player", "src" : returnSrc})); //Characcter image
-    $("#playerDiv").append($('<div>',{ "class" : "col-md-4"})); //Blank Div for padding
+        // Grabs the chosen classes image source, and hp and assigns it into a div sorted with bootstrap grid
+        var returnSrc = characters.filter(obj => obj.val == chosenClass ).map(obj => obj.src);
+        var returnHP = characters.filter(obj => obj.val == chosenClass ).map(obj => obj.hp);
 
-    // Creates a new div to hold the players hp.
-    $("#gameSpace").append($('<div>',{ "class" : "row", "id" : "playerInfo"}));
-    $("#playerInfo").append($('<div>',{ "class" : "col-md-4 blank", }));
-    $("#playerInfo").append($("<p>", { "id" : "playerHpElement", "class" : "text-center col-md-4", "text" : "HP : " + returnHP}));
-    $("#playerInfo").append($('<div>',{ "class" : "col-md-4 blank"}));
+        $("#playerDiv").append($('<div>',{ "class" : "col-md-4"}));  //Blank Div for padding
+        $("#playerDiv").append($('<img>',{ "class" : "col-md-4","id" : "player", "src" : returnSrc})); //Characcter image
+        $("#playerDiv").append($('<div>',{ "class" : "col-md-4"})); //Blank Div for padding
 
-    // Create a blank div to be used for the combat log
-    $("#gameSpace").append($("<div>", { "class" : "row", "id" : "combatLog"}));
-    // Assign something to combatLog div to display it
-    $("#combatLog").append($("<p>", { "class" : "col-md-12 text-center", "id" : "playerText", "text" : "Please Select an Enemy"}));
-    $("#combatLog").append($("<p>", { "class" : "col-md-12 text-center", "id" : "enemyText", "text" : "to Begin"}));
-    // Create Div for Enemy Names
-    $("#gameSpace").append($("<div>", { "class" : "row", "id" : "enemyNames"}));
-    // Create Div for enemy images
-    $("#gameSpace").append($("<div>", { "class" : "row", "id" : "enemiesContainer"}));
-    // Create div that starts blank for the mobs HP to be displayed
-    $("#gameSpace").append($("<div>", { "class" : "row", "id" : "enemyHealth"})); 
-    enemies.forEach(function(){
-        var enemySrc = enemies.map(a => a.src);
-        var enemyVal = enemies.map(a => a.val);
-        var enemyAtt = enemies.map(a => a.att);
-        var enemyHp = enemies.map(a => a.hp);
-        var enemy = enemies.map(a => a.name);
-        $("#enemiesContainer").append($('<img>',{ "class" : "col-md-4 enemy"}));
-        $("#enemiesContainer").find("img").each(function(index){
-            $(this).attr("value", enemyVal[index]);
-        });
-        $("#enemiesContainer").find("img").each(function(index){
-            $(this).attr("src", enemySrc[index]);
-            console.log(enemySrc[index]);
-        });
-        $("#enemyHealth").append($('<p>', { "class" : "text-center col-md-4"}));
-        $("#enemyHealth").find("p").each(function(index){
-            $(this).text("HP : " + enemyHp[index]);
-        });
-        $("#enemyNames").append($('<p>', { "class" : "text-center col-md-4"}));
-        $("#enemyNames").find("p").each(function(index){
-            $(this).text(enemy[index]);
-        });
-    }); 
-});
-$(document).one("click", ".enemy", function(){
-    enemyChosen = true;
-    console.log($(this).attr("value"));
-    chosenEnemy.push($(this).attr("value"));
-    console.log(chosenEnemy);
-    if(enemyChosen == true)
-    {
-        // Delete Visual Elements for enemy selection
-        $("#enemyNames").remove();
-        $("#enemiesContainer").remove();
-        $("#enemyHealth").remove();
+        // Creates a new div to hold the players hp.
+        $("#gameSpace").append($('<div>',{ "class" : "row", "id" : "playerInfo"}));
+        $("#playerInfo").append($('<div>',{ "class" : "col-md-4 blank", }));
+        $("#playerInfo").append($("<p>", { "id" : "playerHpElement", "class" : "text-center col-md-4", "text" : "HP : " + returnHP}));
+        $("#playerInfo").append($('<div>',{ "class" : "col-md-4 blank"}));
 
-        // Clear Combat Log
-        $("#playerText").text("       |        ");
-        $("#enemyText").text("       |        ");
-
-        // Create Target Info Div that Displays Health
-        $("#gameSpace").append($('<div>',{ "class" : "row", "id" : "targetInfo"}));
-        var returnEnmSrc = enemies.filter(obj => obj.val == chosenEnemy ).map(obj => obj.src); // call values from array
-        var returnEnmHP = enemies.filter(obj => obj.val == chosenEnemy ).map(obj => obj.hp); // call values from array
-        
-        // Add div for target HP
-        $("#targetInfo").append($('<div>',{ "class" : "col-md-4 blank", }));
-        $("#targetInfo").append($("<p>", { "id" : "targetHpElement", "class" : "text-center col-md-4", "text" : "HP : " + returnEnmHP}));
-        $("#targetInfo").append($('<div>',{ "class" : "col-md-4 blank"}));
-        
-        // Add div for Target Image
-        $("#gameSpace").append($("<div>", {"class" : "row", "id" : "selectedEnemyContainer"}))
-        $("#selectedEnemyContainer").append($('<div>',{ "class" : "col-md-4"}));  //Blank Div for padding
-        $("#selectedEnemyContainer").append($('<img>',{ "class" : "col-md-4","id" : "target", "src" : returnEnmSrc})); //Characcter image
-        $("#selectedEnemyContainer").append($('<div>',{ "class" : "col-md-4"}));
-        var fds = 100;
-        for(var i = 0; i < 100; i++)
+        // Create a blank div to be used for the combat log
+        $("#gameSpace").append($("<div>", { "class" : "row", "id" : "combatLog"}));
+        // Assign something to combatLog div to display it
+        $("#combatLog").append($("<p>", { "class" : "col-md-12 text-center", "id" : "playerText", "text" : "Please Select an Enemy"}));
+        $("#combatLog").append($("<p>", { "class" : "col-md-12 text-center", "id" : "enemyText", "text" : "to Begin"}));
+        // Create Div for Enemy Names
+        $("#gameSpace").append($("<div>", { "class" : "row", "id" : "enemyNames"}));
+        // Create Div for enemy images
+        $("#gameSpace").append($("<div>", { "class" : "row", "id" : "enemiesContainer"}));
+        // Create div that starts blank for the mobs HP to be displayed
+        $("#gameSpace").append($("<div>", { "class" : "row", "id" : "enemyHealth"})); 
+        enemies.forEach(function(){
+            var enemySrc = enemies.map(a => a.src);
+            var enemyVal = enemies.map(a => a.val);
+            var enemyHp = enemies.map(a => a.hp);
+            var enemy = enemies.map(a => a.name);
+            $("#enemiesContainer").append($('<img>',{ "class" : "col-md-4 enemy"}));
+            $("#enemiesContainer").find("img").each(function(index){
+                $(this).attr("value", enemy[index]);
+            });
+            $("#enemiesContainer").find("img").each(function(index){
+                $(this).attr("src", enemySrc[index]);
+                // console.log(enemySrc[index]);
+            });
+            $("#enemyHealth").append($('<p>', { "class" : "text-center col-md-4"}));
+            $("#enemyHealth").find("p").each(function(index){
+                $(this).text("HP : " + enemyHp[index]);
+            });
+            $("#enemyNames").append($('<p>', { "class" : "text-center col-md-4"}));
+            $("#enemyNames").find("p").each(function(index){
+                $(this).text(enemy[index]);
+            });
+        }); 
+    });
+    $(document).one("click", ".enemy", function(){
+        enemyChosen = true;
+        // console.log($(this).attr("value"));
+        chosenEnemy.push($(this).attr("value"));
+        // console.log(chosenEnemy);
+        if(enemyChosen == true)
         {
-            fds += 100;
-            console.log(fds);
+            // Delete Visual Elements for enemy selection
+            $("#enemyNames").remove();
+            $("#enemiesContainer").remove();
+            $("#enemyHealth").remove();
+
+            // Clear Combat Log
+            $("#playerText").text("       |        ");
+            $("#enemyText").text("       |        ");
+
+            // Create Target Info Div that Displays Health
+            $("#gameSpace").append($('<div>',{ "class" : "row", "id" : "targetInfo"}));
+            var returnEnmSrc = enemies.filter(obj => obj.name == chosenEnemy ).map(obj => obj.src); // call values from array
+            var returnEnmHP = enemies.filter(obj => obj.name == chosenEnemy ).map(obj => obj.hp); // call values from array
+            
+            // Add div for target HP
+            $("#targetInfo").append($('<div>',{ "class" : "col-md-4 blank", }));
+            $("#targetInfo").append($("<p>", { "id" : "targetHpElement", "class" : "text-center col-md-4", "text" : "HP : " + returnEnmHP}));
+            $("#targetInfo").append($('<div>',{ "class" : "col-md-4 blank"}));
+            
+            // Add div for Target Image
+            $("#gameSpace").append($("<div>", {"class" : "row", "id" : "selectedEnemyContainer"}))
+            $("#selectedEnemyContainer").append($('<div>',{ "class" : "col-md-4"}));  //Blank Div for padding
+            $("#selectedEnemyContainer").append($('<img>',{ "class" : "col-md-4","id" : "target", "src" : returnEnmSrc})); //Characcter image
+            $("#selectedEnemyContainer").append($('<div>',{ "class" : "col-md-4"}));
+
+            // console.log(returnEnmHP);
+            
         }
-    }   
-});
- 
+        
+        if( returnEnmHP >= 0)
+        {
+            $(document).on("click", "#target", function(){
+                var returnMinAtt = characters.filter(obj => obj.val == chosenClass ).map(obj => obj.minAtt);
+                var returnMaxAtt = characters.filter(obj => obj.val == chosenClass ).map(obj => obj.maxAtt);
+                console.log(returnEnmHP);
+                var playerRandDamage = Math.floor(Math.random() * (parseInt(returnMaxAtt) - parseInt(returnMinAtt)) + parseInt(returnMinAtt));
+                damageStore.push(playerRandDamage);
+                damageCalc = damageCalc + playerRandDamage;
+                $("#playerText").text("You Deal " + damageCalc + " damage to " + chosenEnemy);
+                
+                $("#enemyText").text("test");
+            });
+        }
+        else
+        {
+            console.log("thing is dead")
+        }
+    });
+}
+else
+{
+    console.log("dead");
+}
